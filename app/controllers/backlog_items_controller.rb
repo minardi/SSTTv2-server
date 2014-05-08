5869a81149b969fd1d99a933bb94854ba2d48d20
Andrey Granat <=>
2014-05-08 16:10:34 +0300

    fix

--- app/controllers/backlog_items_controller.rb
+++ app/controllers/backlog_items_controller.rb
@@ -91,7 +91,6 @@ class BacklogItemsController < ApplicationController
         backlog_item[:info] = info
       end
     end
-    
   end
   
   def get_tasks
@@ -102,7 +101,7 @@ class BacklogItemsController < ApplicationController
   		"story", "sprint",	@sprint_id
   	)
 
-    @stories_all = BacklogItem.where(
+    @stories_without_task = BacklogItem.where(
       "item_type = ? AND (status = ? OR status = ? OR status = ? OR status = ?) AND parent_id = ?",
       "story", "todo", "progress", "verify", "done",  @sprint_id
     )
@@ -118,7 +117,7 @@ class BacklogItemsController < ApplicationController
       @all_tasks.push(@tasks)
   	end
 	  
-    @all_tasks.push(@stories_all)
+    @all_tasks.push(@stories_without_task)
 
     @all_tasks = @all_tasks.flatten
 	
