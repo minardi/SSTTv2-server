ba98bdf87d19c4c0e935f03e53ad5ee7aebcd86a
Andrey Granat <=>
2014-05-11 16:58:42 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ SSTaskTracker::Application.routes.draw do
   get 'backlog_items/get_items/:item_type/:status/:parent_id' => 'backlog_items#get_items'
 
   get 'backlog_items/get_active_sprint/:parent_id' => 'backlog_items#get_active_sprint'
-  put 'backlog_items/get_active_sprint/:parent_id/:id', to: redirect('backlog_items/%{id}')
-  post 'backlog_items/get_active_sprint/:parent_id', to: redirect('backlog_items/%{parent_id}')
-  patch 'backlog_items/get_active_sprint/:parent_id/:id', to: redirect('backlog_items/%{id}')
+  put 'backlog_items/get_active_sprint/:parent_id/:id', to: redirect('/backlog_items/%{id}')
+  patch 'backlog_items/get_active_sprint/:parent_id/:id', to: redirect('/backlog_items/%{id}')
   
   get 'backlog_items/get_tasks/:sprint_id' => 'backlog_items#get_tasks'
 
